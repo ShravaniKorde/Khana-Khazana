6d136b0e8632398525d2e401778671ef133fab32
Shravani Korde <149994109+ShravaniKorde@users.noreply.github.com>
2025-08-13 14:09:28 +0530

    updated changes

---
+++ src/main/java/com/example/restro/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.restro.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// This is a controller class for handling user related requests.
+@RestController
+public class UserController {
+
+}
--- src/main/java/com/example/restro/entity/RestaurantDetails.java
+++ src/main/java/com/example/restro/entity/RestaurantDetails.java
@@ -5,7 +5,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "restuarant")
+@Table(name = "restaurant")
 public class RestaurantDetails {
 
     @Id
